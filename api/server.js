73bc3ee7b45f2cf25c97add6571231cf0157410a
AsulKHAOS <AsulKHAOS@proton.me>
2026-02-13 22:24:13 -0300

    Update server.js

--- api/server.js
+++ api/server.js
@@ -21,3 +21,22 @@ app.post("/bot/:id/command", async (req,res)=>{
 });
 
 app.listen(3000, ()=>console.log("API rodando"));
+
+const Plugin = require("./models/Plugin");
+
+app.get("/plugins", async (req,res)=>{
+  const plugins = await Plugin.find();
+  res.json(plugins);
+});
+
+app.post("/bot/:botId/install/:pluginId", async (req,res)=>{
+  const bot = await Bot.findById(req.params.botId);
+  const plugin = await Plugin.findById(req.params.pluginId);
+
+  bot.commands.push(...plugin.commands);
+
+  await bot.save();
+  await registerSlash(bot);
+
+  res.json({status:"plugin installed"});
+});
