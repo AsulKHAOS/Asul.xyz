19c9b08da084a292b913434cb72e5758c83f3a1c
AsulKHAOS <AsulKHAOS@proton.me>
2026-02-13 22:17:32 -0300

    Create botManager.js

---
+++ bot/botManager.js
@@ -0,0 +1,31 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const bots = new Map();
+
+async function startBot(botData) {
+  if (bots.has(botData.clientId)) {
+    bots.get(botData.clientId).destroy();
+  }
+
+  const client = new Client({
+    intents: [
+      GatewayIntentBits.Guilds,
+      GatewayIntentBits.GuildMessages,
+      GatewayIntentBits.GuildMembers,
+      GatewayIntentBits.MessageContent
+    ]
+  });
+
+  client.once("ready", () => {
+    console.log("Bot online:", client.user.tag);
+  });
+
+  require("./handlers/interactionHandler")(client, botData);
+  require("./handlers/eventHandler")(client, botData);
+
+  await client.login(botData.token);
+
+  bots.set(botData.clientId, client);
+}
+
+module.exports = { startBot, bots };
